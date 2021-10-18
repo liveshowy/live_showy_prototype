79bb4d96dd02986434ee3e160376b101465ced29
Owen Bickford <owen+github@owencode.com>
2021-10-17 21:09:26 -0400

    add prelim pubsub, handle `mouse-event`

--- lib/loom_web/live/stage_live/index.ex
+++ lib/loom_web/live/stage_live/index.ex
@@ -3,11 +3,39 @@ defmodule LoomWeb.StageLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :coords, %{"clientX" => 0, "clientY" => 0})}
+    if connected?(socket), do: subscribe()
+    coords = %{"clientX" => 0, "clientY" => 0}
+    broadcast(:joined, coords)
+    {:ok, assign(socket, :coords, coords)}
+  end
+
+  @impl true
+  def handle_info({:joined, coords}, socket) do
+    {:noreply, assign(socket, :coords, coords)}
+  end
+
+  @impl true
+  def handle_info({:event, coords}, socket) do
+    {:noreply, assign(socket, :coords, coords)}
   end
 
   @impl true
   def handle_event("touch-event", params, socket) do
+    broadcast(:event, params)
     {:noreply, assign(socket, :coords, params)}
+  end
+
+  @impl true
+  def handle_event("mouse-event", params, socket) do
+    broadcast(:event, params)
+    {:noreply, assign(socket, :coords, params)}
+  end
+
+  def subscribe do
+    Phoenix.PubSub.subscribe(Loom.PubSub, "stage")
+  end
+
+  defp broadcast(event, coords) do
+    Phoenix.PubSub.broadcast(Loom.PubSub, "stage", {event, coords})
   end
 end
