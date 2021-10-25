4efe79982d239b09162301ab82ebc38efff00df1
Owen Bickford <owen+github@owencode.com>
2021-10-25 09:19:08 -0400

    add presence (WIP)

--- lib/loomer_web/live/stage_live/index.ex
+++ lib/loomer_web/live/stage_live/index.ex
@@ -3,19 +3,35 @@ defmodule LoomerWeb.StageLive.Index do
 
   use LoomerWeb, :live_view
 
+  alias LoomerWeb.Presence
+
+  @topic "stage"
+
   @impl true
   def mount(_params, _session, socket) do
     if connected?(socket), do: subscribe()
 
-    initial_state = [
-      x: "50%",
-      y: "50%",
-      username: Faker.Internet.user_name(),
-      color: Faker.Color.rgb_hex()
-    ]
+    username = Faker.Internet.user_name()
 
-    broadcast(:joined, initial_state)
-    {:ok, assign(socket, initial_state)}
+    Presence.track(
+      self(),
+      @topic,
+      username,
+      %{
+        x: "50%",
+        y: "50%",
+        width: "1000",
+        height: "1000",
+        color: "#" <> Faker.Color.rgb_hex(),
+        username: username
+      }
+    )
+
+    users =
+      Presence.list(@topic) |> Enum.map(fn {_user_id, data} -> data[:metas] |> List.first() end)
+
+    # broadcast(:joined, users: users)
+    {:ok, assign(socket, users: users, current_user: username)}
   end
 
   @impl true
@@ -25,20 +41,42 @@ defmodule LoomerWeb.StageLive.Index do
 
   @impl true
   def handle_info({:dot_moved, state}, socket) do
-    IO.inspect(state)
     {:noreply, socket}
+  end
+
+  def handle_info(%{event: "presence_diff", payload: _payload}, socket) do
+    users =
+      Presence.list(@topic) |> Enum.map(fn {_user_id, data} -> data[:metas] |> List.first() end)
+
+    {:noreply, assign(socket, users: users)}
   end
 
   @impl true
   def handle_event(event, [x, y], socket)
       when event in ["touch-event", "mouse-event"] do
-    new_socket =
-      socket
-      |> update(:x, fn _ -> x end)
-      |> update(:y, fn _ -> y end)
+    current_user = get_current_user(socket)
 
-    broadcast(:dot_moved, new_socket.assigns)
-    {:noreply, new_socket}
+    metas =
+      Presence.get_by_key(@topic, current_user)[:metas]
+      |> List.first()
+      |> Map.merge(%{x: x, y: y})
+
+    Presence.update(self(), @topic, current_user, metas)
+
+    # broadcast(:dot_moved, new_socket.assigns)
+    {:noreply, socket}
+  end
+
+  def handle_event("element-dimensions", [width, height], socket) do
+    current_user = get_current_user(socket)
+
+    metas =
+      Presence.get_by_key(@topic, current_user)[:metas]
+      |> List.first()
+      |> Map.merge(%{width: width, height: height})
+
+    Presence.update(self(), @topic, current_user, metas)
+    {:noreply, socket}
   end
 
   def handle_event(_event, _params, socket) do
@@ -46,10 +84,14 @@ defmodule LoomerWeb.StageLive.Index do
   end
 
   def subscribe do
-    Phoenix.PubSub.subscribe(Loomer.PubSub, "stage")
+    Phoenix.PubSub.subscribe(Loomer.PubSub, @topic)
   end
 
   defp broadcast(event, state) do
-    Phoenix.PubSub.broadcast(Loomer.PubSub, "stage", {event, state})
+    Phoenix.PubSub.broadcast(Loomer.PubSub, @topic, {event, state})
+  end
+
+  defp get_current_user(%{assigns: %{current_user: current_user}}) do
+    current_user
   end
 end
