7ce55aece3b2eb29dca6976493914edcc66fbebe
Owen Bickford <owen@smartlogic.io>
2021-11-18 12:51:43 -0500

    make portmidi avail in all environments again

--- mix.exs
+++ mix.exs
@@ -48,7 +48,7 @@ defmodule Loomer.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:faker, "~> 0.16.0"},
       {:uuid, "~> 1.1"},
-      {:portmidi, git: "https://github.com/thbar/ex-portmidi.git", only: [:prod, :dev]},
+      {:portmidi, git: "https://github.com/thbar/ex-portmidi.git"},
       {:credo, "~> 1.5.6"}
     ]
   end
