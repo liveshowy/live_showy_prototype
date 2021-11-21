f64a0fd61125da7159df504326b7ed8b67c4e714
Owen Bickford <owen@owencode.com>
2021-11-21 18:33:22 -0500

    add BandLive component

---
+++ lib/loomer_web/live/band_live/index.ex
@@ -0,0 +1,55 @@
+defmodule LoomerWeb.BandLive.Index do
+  @moduledoc """
+  A stage for up to six members to collaborate.
+  """
+  use LoomerWeb, :live_view
+  alias LoomerWeb.Presence
+  alias LoomerWeb.Live.Components.LatencyMonitor
+  alias LoomerWeb.Live.Components.Keyboard
+
+  @topic "band_live"
+
+  @impl true
+  def mount(
+        _params,
+        _session,
+        %{assigns: %{current_user_id: current_user_id}} = socket
+      ) do
+    if connected?(socket), do: subscribe()
+    current_user = Loomer.Users.get_user(current_user_id)
+
+    Presence.track(
+      self(),
+      @topic,
+      current_user.id,
+      current_user
+    )
+
+    users =
+      Presence.list(@topic) |> Enum.map(fn {_user_id, data} -> data[:metas] |> List.first() end)
+
+    {:ok, assign(socket, users: users, current_username: current_user.username)}
+  end
+
+  def handle_event("note-on", [note, velocity], socket) do
+    IO.inspect([note, velocity], label: "NOTE ON")
+    output = Loomer.MidiDevices.get_device(:output)
+    PortMidi.write(output, {144, note, velocity})
+    {:noreply, socket}
+  end
+
+  def handle_event("note-off", note, socket) do
+    IO.inspect(note, label: "NOTE OFF")
+    output = Loomer.MidiDevices.get_device(:output)
+    PortMidi.write(output, {144, note, 0})
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event(event, params, socket) do
+    IO.inspect(params, label: event)
+    {:noreply, socket}
+  end
+
+  defp subscribe, do: Phoenix.PubSub.subscribe(Loomer.PubSub, @topic)
+end
