010c3ef8a21d1cfb3f9fd068f5dec9aee9444eae
Owen Bickford <owen@owencode.com>
2021-11-21 13:22:22 -0500

    add border and xy lines

--- lib/loomer_web/live/components/x_y_pad.ex
+++ lib/loomer_web/live/components/x_y_pad.ex
@@ -6,16 +6,21 @@ defmodule LoomerWeb.Live.Components.XYPad do
   def render(assigns) do
     ~H"""
       <svg
-      class="w-[350px] h-[350px] bg-purple-800 rounded-lg"
+      class="w-[350px] h-[350px] bg-purple-800 rounded-lg border-2 border-purple-700 shadow-lg"
       id="touchpad"
       phx-hook="TrackTouchEvents"
       viewBox="0 0 350 350"
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
     >
+      <line x1="0" x2="350" y1="175" y2="175" stroke="rgb(109, 40, 217)" stroke-width="2" />
+      <line x1="175" x2="175" y1="0" y2="350" stroke="rgb(109, 40, 217)" stroke-width="2" />
+
       <%= for user <- @users do %>
-        <.live_component module={UserDot} id={user.id <> "-dot"} user={user} />
+        <.live_component module={UserDot} id={user.id <> "-dot"} user={user} current_user_id={@current_user_id} />
       <% end %>
+
+      <use xlink:href={"#" <> @current_user_id <> "-dot"} />
     </svg>
     """
   end
