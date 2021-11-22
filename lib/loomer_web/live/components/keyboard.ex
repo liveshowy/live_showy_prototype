4e793e2e8bfe5f258c289ff584dd8fbd91d9e0c4
Owen Bickford <owen@owencode.com>
2021-11-21 21:11:30 -0500

    add .select-none

--- lib/loomer_web/live/components/keyboard.ex
+++ lib/loomer_web/live/components/keyboard.ex
@@ -3,7 +3,7 @@ defmodule LoomerWeb.Live.Components.Keyboard do
 
   def render(assigns) do
     ~H"""
-    <div class="flex gap-1">
+    <div class="flex gap-1 select-none">
       <%= for note <- 0..127 do %>
         <button type="button" phx-hook="HandleKeyboardPresses" id={"key-#{note}"} class="flex items-end justify-center w-16 h-64 p-1 font-bold text-purple-600 bg-white rounded" value={note}>
           <%= note %>
