b83929281c40e87b3523d1cba646ca88dbcc4e94
Owen Bickford <owen@owencode.com>
2021-11-27 13:41:21 -0500

    move key into keyboard component

--- lib/loomer_web/live/components/keyboard.ex
+++ lib/loomer_web/live/components/keyboard.ex
@@ -1,24 +1,30 @@
 defmodule LoomerWeb.Live.Components.Keyboard do
+  @moduledoc """
+  - Create a range of notes from 0 to 126
+  - Group notes into octaves (12 notes each)
+  - Index each note within the octaves
+  - Index the list of octaves
+  """
   use Phoenix.LiveComponent
-  alias LoomerWeb.Live.Components.KeyboardKey
 
-  # Create a range of notes from 0 to 126
-  # Group notes into octaves (12 notes each)
-  # Index each note within the octaves
-  # Index the list of octaves
-  @notes 0..126
+  @notes 0..127
          |> Enum.chunk_every(12)
          |> Enum.map(&Enum.with_index/1)
          |> Enum.with_index()
 
-  def render(%{octave: octave} = assigns) do
-    {notes, _index} = @notes |> Enum.at(octave, [])
+  @default_class "flex items-end justify-center w-20 h-64 p-1 font-bold transition-color duration-150 rounded-b-xl"
 
+  def update(assigns, socket) do
+    {notes, _index} = @notes |> Enum.at(assigns.octave, [])
+    {:ok, assign(socket, octave: assigns.octave, notes: notes)}
+  end
+
+  def render(assigns) do
     ~H"""
     <div>
       <div class="flex gap-1 p-2 pt-0 shadow-lg select-none bg-gradient-to-b from-purple-800 to-purple-600 rounded-xl">
-        <%= for {note, index} <- notes do %>
-          <.live_component
+        <%= for {note, index} <- @notes do %>
+          <.key
             id={"keyboard-key-#{note}"}
             module={KeyboardKey}
             color={get_key_color(index)}
@@ -27,6 +33,26 @@ defmodule LoomerWeb.Live.Components.Keyboard do
         <% end %>
       </div>
     </div>
+    """
+  end
+
+  defp get_class(:black),
+    do: @default_class <> " bg-gradient-to-b from-gray-800 to-gray-900 text-white active:to-black"
+
+  defp get_class(:white),
+    do:
+      @default_class <>
+        " bg-gradient-to-b from-gray-100 to-white text-black active:to-gray-300"
+
+  defp key(assigns) do
+    ~H"""
+    <button
+      type="button"
+      phx-hook="HandleKeyboardPresses"
+      value={@note}
+      id={"key-#{@note}"}
+      class={get_class(@color)}
+    ></button>
     """
   end
 
