553d9eff39364764f5fded04743a513903bce0e7
Owen Bickford <owen@owencode.com>
2021-12-05 14:47:32 -0500

    add os_mon to extra apps for live dashboard

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule LiveShowy.MixProject do
   def application do
     [
       mod: {LiveShowy.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
