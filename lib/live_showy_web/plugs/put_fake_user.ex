a2e99f3d3c272a1daa189849d0b310ec7378a2f4
Owen Bickford <owen@owencode.com>
2021-12-05 17:14:53 -0500

    ignore unused user_id

--- lib/live_showy_web/plugs/put_fake_user.ex
+++ lib/live_showy_web/plugs/put_fake_user.ex
@@ -8,7 +8,7 @@ defmodule LiveShowyWeb.Plugs.PutFakeUser do
 
   def call(conn, _opts) do
     with current_user_id <- get_session(conn, "current_user_id"),
-         %{id: user_id} <- LiveShowy.Users.get_user(current_user_id) do
+         %{id: _user_id} <- LiveShowy.Users.get_user(current_user_id) do
       conn
     else
       _ ->
