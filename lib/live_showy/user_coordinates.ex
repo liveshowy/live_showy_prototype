a9a73b9046d01562906f46856b8bf0d58f2da795
Owen Bickford <owen@owencode.com>
2021-12-11 21:10:21 -0500

    document fns, generate random coords if none exist

--- lib/live_showy/user_coordinates.ex
+++ lib/live_showy/user_coordinates.ex
@@ -23,21 +23,34 @@ defmodule LiveShowy.UserCoordinates do
     {:ok, nil}
   end
 
+  @doc """
+  Store XY coordinates for a user.
+  """
   def put_coords(user_id, [x, y] = coords) when is_number(x) and is_number(y) do
     :ets.insert(__MODULE__, {user_id, coords})
+    [x, y]
   end
 
+  @doc """
+  Get a user's coordinates. If none exist, generate random coords.
+  """
   def get_coords(user_id) do
     case :ets.lookup(__MODULE__, user_id) do
       [{^user_id, coords}] -> coords
-      [] -> nil
+      [] -> put_coords(user_id, [Enum.random(0..90), Enum.random(0..90)])
     end
   end
 
+  @doc """
+  Remove a user's coordinates.
+  """
   def remove_coords(user_id) do
     :ets.delete(__MODULE__, user_id)
   end
 
+  @doc """
+  List all user coordinates.
+  """
   def list_coords, do: :ets.tab2list(__MODULE__)
   def get_first_key, do: :ets.first(__MODULE__)
   def get_next_key(previous_key), do: :ets.next(__MODULE__, previous_key)
