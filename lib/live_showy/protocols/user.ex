4d28e201f27848c55a1498823829b0df800ec188
Owen Bickford <owen@owencode.com>
2021-12-11 20:14:22 -0500

    remove coords from user structs, remove guest user

--- lib/live_showy/protocols/user.ex
+++ lib/live_showy/protocols/user.ex
@@ -1,8 +1,8 @@
 defprotocol LiveShowy.Protocols.User do
   @fallback_to_any true
 
-  @enforce_keys [:id, :username, :color, :x, :y, :token]
-  defstruct [:id, :username, :color, :x, :y, :token]
+  @enforce_keys [:id, :username, :color, :token]
+  defstruct [:id, :username, :color, :token]
 
   @spec new(binary()) :: User.t()
   def new(username)
--- lib/live_showy/users/fake.ex
+++ lib/live_showy/users/fake.ex
@@ -3,8 +3,8 @@ defmodule LiveShowy.Users.Fake do
   The most basic form of a user.
   """
 
-  @enforce_keys [:id, :username, :x, :y, :color, :token]
-  defstruct [:id, :username, :x, :y, :color, :token]
+  @enforce_keys [:id, :username, :color, :token]
+  defstruct [:id, :username, :color, :token]
 
   @doc """
   Returns a fake user with a random UUID and username.
@@ -14,18 +14,14 @@ defmodule LiveShowy.Users.Fake do
       id: UUID.uuid4(),
       username: Faker.Internet.user_name(),
       color: "#" <> Faker.Color.rgb_hex(),
-      x: Enum.random(10..90) |> Integer.to_string() |> String.pad_leading(3, "0"),
-      y: Enum.random(10..90) |> Integer.to_string() |> String.pad_leading(3, "0"),
       token: nil
     }
   end
 
   alias LiveShowy.Users.Fake
-  alias LiveShowy.Users.Guest
   alias LiveShowy.Protocols.User
 
-  defimpl User, for: Atom do
-    def new(:fake), do: Fake.new()
-    def new(_), do: Guest.new()
+  defimpl User, for: Any do
+    def new(_), do: Fake.new()
   end
 end
