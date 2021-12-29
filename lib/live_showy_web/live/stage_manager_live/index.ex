7c45c9ca708606934ebef7e7843120064d6092b7
Owen Bickford <owen@owencode.com>
2021-12-29 13:33:59 -0500

    add users list, add prelim event and info handlers

--- lib/live_showy_web/live/stage_manager_live/index.ex
+++ lib/live_showy_web/live/stage_manager_live/index.ex
@@ -3,9 +3,35 @@ defmodule LiveShowyWeb.StageManagerLive.Index do
   Users with a stage manager role may access this LiveView to coordinate other users and performances.
   """
   use LiveShowyWeb, :live_view
+  alias LiveShowy.Users
+  alias LiveShowyWeb.Live.Components.Users, as: UsersList
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    if connected?(socket), do: subscribe()
+    {:ok, assign(socket, users: get_users())}
   end
+
+  @impl true
+  def handle_info({action, _user}, socket)
+      when action in [:user_added, :user_updated, :user_removed] do
+    {:noreply, assign(socket, users: get_users())}
+  end
+
+  def handle_info(message, socket) do
+    IO.inspect(message, label: "UNKNOWN INFO MESSAGE")
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event(event, _params, socket) do
+    IO.inspect(event, label: "UNKNDOWN EVENT")
+    {:noreply, socket}
+  end
+
+  defp subscribe do
+    Phoenix.PubSub.subscribe(LiveShowy.PubSub, LiveShowy.Users.get_topic())
+  end
+
+  defp get_users, do: Users.list_users() |> Enum.map(fn {_id, user} -> user end)
 end
