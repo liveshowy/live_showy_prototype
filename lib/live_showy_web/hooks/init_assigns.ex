a3ea349da188a7d4d4b946e797b0445a98a12bd2
Owen Bickford <owen@owencode.com>
2022-01-06 11:31:16 -0500

    fix fn order

--- lib/live_showy_web/hooks/init_assigns.ex
+++ lib/live_showy_web/hooks/init_assigns.ex
@@ -2,14 +2,13 @@ defmodule LiveShowyWeb.InitAssigns do
   @moduledoc """
   Ensures common assigns are applied to all LiveViews attaching to this hook.
   """
-
   import Phoenix.LiveView
-
-  def on_mount(:default, _params, _session, socket) do
-    {:cont, socket}
-  end
 
   def on_mount(:user, _params, %{"current_user_id" => current_user_id} = _session, socket) do
     {:cont, assign(socket, current_user_id: current_user_id)}
+  end
+
+  def on_mount(:default, _params, _session, socket) do
+    {:cont, socket}
   end
 end
