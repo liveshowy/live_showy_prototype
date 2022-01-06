48f9f41a512aae0fb30a921addaac8a99fd9d4c5
Owen Bickford <owen@owencode.com>
2022-01-06 11:20:31 -0500

    subscribe to wifi updates

--- lib/live_showy_web/live/landing_live/index.ex
+++ lib/live_showy_web/live/landing_live/index.ex
@@ -3,17 +3,22 @@ defmodule LiveShowyWeb.LandingLive.Index do
   The landing page for LiveShowy.
   """
   use LiveShowyWeb, :live_view
-  alias LiveShowyWeb.Live.Components.WifiCard
-  alias LiveShowyWeb.Live.Components.LatencyMonitor
+  alias LiveShowy.Wifi
+  alias LiveShowyWeb.Live.Components.WifiBar
 
   @impl true
   def mount(_params, _session, socket) do
+    if connected?(socket), do: subscribe()
     {:ok, socket}
+  end
+
+  defp subscribe do
+    Phoenix.PubSub.subscribe(LiveShowy.PubSub, Wifi.get_topic())
   end
 
   @impl true
   def handle_info({:wifi_credential_updated, {key, value}}, socket) do
-    send_update(WifiCard, [{key, value}, id: "wifi-info-stage-manager"])
+    send_update(WifiBar, [{key, value}, id: "wifi-bar"])
     {:noreply, socket}
   end
 end
