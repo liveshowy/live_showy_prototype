38a5263089a85e2dfed7608be76202261d1aca5f
Owen Bickford <owen@owencode.com>
2022-01-06 20:20:11 -0500

    log important midi device actions

--- lib/live_showy/midi_devices.ex
+++ lib/live_showy/midi_devices.ex
@@ -3,6 +3,7 @@ defmodule LiveShowy.MidiDevices do
   A GenServer for managing with MIDI devices connected to a server.
   """
   use GenServer
+  require Logger
 
   def start_link(_state) do
     GenServer.start_link(__MODULE__, nil, name: __MODULE__)
@@ -34,7 +35,16 @@ defmodule LiveShowy.MidiDevices do
   Places a list of devices from PortMidi into ETS.
   """
   def set_devices() do
-    :ets.insert(__MODULE__, {:devices, PortMidi.devices()})
+    devices = PortMidi.devices()
+    :ets.insert(__MODULE__, {:devices, devices})
+
+    for input <- devices.input do
+      Logger.info(midi_device_added: input)
+    end
+
+    for output <- devices.output do
+      Logger.info(midi_device_added: output)
+    end
 
     {:ok, :ets.lookup(__MODULE__, :devices)}
   end
@@ -52,8 +62,10 @@ defmodule LiveShowy.MidiDevices do
   Places an input or output device PID in ETS.
   """
   def set_device(device_type, device_name) when device_type in [:input, :output] do
-    {:ok, device} = PortMidi.open(device_type, device_name)
-    :ets.insert(__MODULE__, {device_type, device})
+    {:ok, pid} = PortMidi.open(device_type, device_name)
+    :ets.insert(__MODULE__, {device_type, pid})
+
+    Logger.info(midi_device_set: {device_type, device_name, pid})
 
     {:ok, :ets.lookup(__MODULE__, device_type)}
   end
@@ -73,6 +85,8 @@ defmodule LiveShowy.MidiDevices do
   """
   def remove_device(device) when device in [:input, :output] do
     :ets.delete(__MODULE__, device)
+
+    Logger.info(midi_device_removed: device)
 
     {:ok}
   end
