7f13479cfdf23a38446b4d3a94b0a1cbbef7eded
Owen Bickford <owen@owencode.com>
2022-01-17 17:20:19 -0500

    indicate activity with a dot

--- lib/live_showy_web/live/components/client_midi_devices.ex
+++ lib/live_showy_web/live/components/client_midi_devices.ex
@@ -5,7 +5,10 @@ defmodule LiveShowyWeb.Components.ClientMidiDevices do
     ~H"""
     <ul>
       <%= for device <- @devices do %>
-        <li class={if device["id"] in @playing_devices, do: "text-green-300"}><%= device["name"] %></li>
+        <li class="flex items-center gap-2">
+          <span class={"rounded-full w-4 h-4 shadow-inner #{if device["id"] in @playing_devices, do: "bg-green-500", else: "bg-purple-600"}"}></span>
+          <span><%= device["name"] %></span>
+        </li>
       <% end %>
     </ul>
     """
