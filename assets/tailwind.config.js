8ee1078f0b6ac7d5eb18ff4ef2a2b9422c899b03
Owen Bickford <owen@owencode.com>
2022-01-17 21:05:52 -0500

    udpate tailwind config, add color aliases

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,8 +1,17 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
-  mode: "jit",
-  purge: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
+  content: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
   theme: {
     extend: {
+      colors: {
+        brand: colors.violet,
+        success: colors.emerald,
+        warning: colors.orange,
+        danger: colors.rose,
+        info: colors.sky,
+        gray: colors.stone,
+      },
       boxShadow: {
         "inner-lg": "inset 0 10px 10px -5px rgb(0 0 0 / 0.1), inset 0 -10px 10px -5px rgb(0 0 0 / 0.1)"
       },
@@ -11,7 +20,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [
-    require("@tailwindcss/aspect-ratio"),
-  ],
+  plugins: [],
 }
