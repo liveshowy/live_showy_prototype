930bf9a049b63dc2b51d84a317649f732f21c172
Owen Bickford <owen@owencode.com>
2022-01-19 19:10:48 -0500

    update alert component

--- lib/live_showy_web/components/alert.ex
+++ lib/live_showy_web/components/alert.ex
@@ -4,11 +4,23 @@ defmodule LiveShowyWeb.Components.Alert do
 
   prop flash, :map, default: nil
 
+  prop classes, :css_class,
+    default:
+      "animate-fade-in-slide-down w-full rounded shadow-md text-white p-4 mx-auto max-w-7xl"
+
   def render(assigns) do
     ~F"""
     {#if @flash != %{} }
-      <aside class={"rounded shadow-md text-white", "bg-danger-700": @flash["error"], "bg-info-600": @flash["info"]} phx-click="lv:clear-flash" phx-value-key="info">
-        <p class="p-4 mx-auto max-w-7xl">{ inspect(@flash, pretty: true) }</p>
+      <aside
+        class={
+          @classes,
+          "bg-danger-700": @flash["error"],
+          "bg-info-600": @flash["info"]
+        }
+        phx-click="lv:clear-flash"
+        phx-value-key={if @flash["error"], do: "error", else: "info"}
+      >
+        <p>{ @flash["error"] || @flash["info"] }</p>
       </aside>
     {/if}
     """
