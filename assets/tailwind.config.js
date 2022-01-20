455992ff45dc1af46859665078b4975c63627be3
Owen Bickford <owen@owencode.com>
2022-01-19 19:10:34 -0500

    add animations

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -15,6 +15,21 @@ module.exports = {
       boxShadow: {
         "inner-lg": "inset 0 10px 10px -5px rgb(0 0 0 / 0.1), inset 0 -10px 10px -5px rgb(0 0 0 / 0.1)"
       },
+      keyframes: {
+        'fade-in': {
+          from: {opacity: 0},
+          to: {opacity: 100},
+        },
+        'fade-in-slide-down': {
+          from: { opacity: 0, transform: 'translateY(-0.2rem)'},
+          to: { opacity: 1, transform: 'initial'},
+        },
+      },
+      animation: {
+        'fade-in': 'fade-in 500ms ease-in-out',
+        'fade-in-slide-down': 'fade-in-slide-down 250ms ease-in-out',
+        'fade-out-slide-up': 'fade-in-slide-down 100ms ease-in-out reverse',
+      },
     },
   },
   variants: {
