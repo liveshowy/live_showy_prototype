dbccef565a81a6baef5d905c3bdc49cff5951104
Owen Bickford <owen@owencode.com>
2022-01-23 13:54:18 -0500

    improve code formatting

--- lib/live_showy_web/components/chat_form.ex
+++ lib/live_showy_web/components/chat_form.ex
@@ -12,8 +12,16 @@ defmodule LiveShowyWeb.Components.ChatForm do
   def render(assigns) do
     ~F"""
     <Form for={:message} submit={@submit} opts={autocomplete: "off"} class="flex gap-1">
-      <TextInput name="body" value={@message.body} class="flex-grow px-2 py-1 transition focus:bg-white focus:text-black focus:outline-none bg-brand-700" opts={autofocus: true, required: true} />
+
+      <TextInput
+        name="body"
+        value={@message.body}
+        class="flex-grow px-2 py-1 transition focus:bg-white focus:text-black focus:outline-none bg-brand-700"
+        opts={autofocus: true, required: true}
+      />
+
       <Button type="submit" click={nil} label="SEND" rounded="rounded-none" />
+
     </Form>
     """
   end
