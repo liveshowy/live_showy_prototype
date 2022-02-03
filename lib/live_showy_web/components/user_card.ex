21ed8155044df6d44625713d19d3b6665e1e39b3
Owen Bickford <owen@owencode.com>
2022-02-03 11:20:35 -0500

    add user card

---
+++ lib/live_showy_web/components/user_card.ex
@@ -0,0 +1,40 @@
+defmodule LiveShowyWeb.Components.UserCard do
+  @moduledoc false
+  use Surface.Component
+  alias LiveShowyWeb.Components.Card
+
+  prop user, :struct, required: true
+  prop transparent, :boolean
+  prop shadow, :string
+  prop padding, :string
+  prop class, :css_class
+
+  def render(assigns) do
+    ~F"""
+    <Card transparent={@transparent} shadow={@shadow} padding={@padding} class={@class}>
+      <:body>
+        <dl class="grid grid-cols-1 gap-1 lg:grid-cols-2">
+          <dt class="font-bold uppercase text-default-500 dark:text-default-400">Username</dt>
+          <dd class="font-mono text-sm break-normal">{@user.username}</dd>
+
+          <dt class="font-bold uppercase text-default-500 dark:text-default-400">Instrument</dt>
+          <dd class="font-mono text-sm break-normal">
+            {if @user.assigned_instrument do
+              Atom.to_string(@user.assigned_instrument.component)
+              |> String.replace(~r/.*\.(\w+)$/, "\\g{1}")
+            end}
+          </dd>
+
+          <dt class="font-bold uppercase text-default-500 dark:text-default-400">Roles</dt>
+          <dd class="font-mono text-sm break-normal">
+            {
+              Enum.map(@user.roles || [], &Atom.to_string/1)
+              |> Enum.join(", ")
+            }
+          </dd>
+        </dl>
+      </:body>
+    </Card>
+    """
+  end
+end
