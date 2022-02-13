90c312158f552e9085c94632a2c111a53ee62218
Owen Bickford <owen@owencode.com>
2022-02-13 10:55:51 -0500

    update layout

--- lib/live_showy_web/components/user_bar.ex
+++ lib/live_showy_web/components/user_bar.ex
@@ -1,34 +1,36 @@
 defmodule LiveShowyWeb.Components.UserBar do
   @moduledoc false
   use Surface.Component
-  alias LiveShowyWeb.Components.LatencyMonitor
+  # alias LiveShowyWeb.Components.LatencyMonitor
 
   prop user, :struct, required: true
 
   def render(assigns) do
     ~F"""
-    <dl class="flex justify-end gap-2 px-2 py-1 overflow-x-auto bg-black bg-opacity-5 whitespace-nowrap">
-      <dt class="font-bold uppercase text-default-500 dark:text-default-400">Username</dt>
-      <dd class="font-mono text-sm break-normal">{@user.username}</dd>
+    <div class="overflow-x-auto bg-black bg-opacity-5">
+      <dl class="flex self-end justify-end w-full gap-2 px-4 py-1 mx-auto overflow-x-auto whitespace-nowrap max-w-screen-2xl">
+        <dt class="font-bold uppercase text-default-500 dark:text-default-400">Username</dt>
+        <dd class="font-mono text-sm break-normal">{@user.username}</dd>
 
-      <dt class="font-bold uppercase text-default-500 dark:text-default-400">Instrument</dt>
-      <dd class="font-mono text-sm">
-        {if @user.assigned_instrument do
-          Atom.to_string(@user.assigned_instrument.component)
-          |> String.replace(~r/.*\.(\w+)$/, "\\g{1}")
-        end}
-      </dd>
+        <dt class="font-bold uppercase text-default-500 dark:text-default-400">Instrument</dt>
+        <dd class="font-mono text-sm">
+          {if @user.assigned_instrument do
+            Atom.to_string(@user.assigned_instrument.component)
+            |> String.replace(~r/.*\.(\w+)$/, "\\g{1}")
+          end}
+        </dd>
 
-      <dt class="font-bold uppercase text-default-500 dark:text-default-400">Roles</dt>
-      <dd class="font-mono text-sm">{Enum.map(@user.roles || [], &Atom.to_string/1) |> Enum.join(", ")}</dd>
+        <dt class="font-bold uppercase text-default-500 dark:text-default-400">Roles</dt>
+        <dd class="font-mono text-sm">{Enum.map(@user.roles || [], &Atom.to_string/1) |> Enum.join(", ")}</dd>
 
-      <!--
-      <dt class="font-bold uppercase text-default-500 dark:text-default-400">Latency</dt>
-      <dd class="font-mono text-sm">
-        <LatencyMonitor id="latency-monitor" />
-      </dd>
-      -->
-    </dl>
+        <!--
+        <dt class="font-bold uppercase text-default-500 dark:text-default-400">Latency</dt>
+        <dd class="font-mono text-sm">
+          <LatencyMonitor id="latency-monitor" />
+        </dd>
+        -->
+      </dl>
+    </div>
     """
   end
 end
