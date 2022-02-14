a43f5b9b953bcb892f1e9166880bed15685b767e
Owen Bickford <owen@owencode.com>
2022-02-13 19:55:27 -0500

    rm unused alias, extra variable

--- lib/live_showy_web/session_hooks/init_assigns.ex
+++ lib/live_showy_web/session_hooks/init_assigns.ex
@@ -3,19 +3,16 @@ defmodule LiveShowyWeb.SessionHooks.InitAssigns do
   Ensures common assigns are applied to all LiveViews attaching to this hook.
   """
   import Phoenix.LiveView
-  alias LiveShowyWeb.Router.Helpers, as: Routes
   alias LiveShowy.UserRoles
   alias LiveShowy.UserInstruments
 
   def on_mount(:user, _params, %{"current_user_id" => current_user_id} = _session, socket) do
-    current_user =
-      LiveShowy.Users.get(current_user_id)
-      |> Map.put_new(:roles, UserRoles.get(current_user_id))
-      |> Map.put_new(:assigned_instrument, UserInstruments.get(current_user_id) |> elem(1))
-
     {:cont,
      assign(socket,
-       current_user: current_user
+       current_user:
+         LiveShowy.Users.get(current_user_id)
+         |> Map.put_new(:roles, UserRoles.get(current_user_id))
+         |> Map.put_new(:assigned_instrument, UserInstruments.get(current_user_id) |> elem(1))
      )}
   end
 
