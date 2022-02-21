b4c0d5eaf1e78b71c0d1a3c1f5f60cc6c6bf7a8b
Owen Bickford <owen+github@owencode.com>
2022-02-21 18:12:50 -0500

    add readonly and disabled appearance, sorted props

--- lib/live_showy_web/components/forms/input.ex
+++ lib/live_showy_web/components/forms/input.ex
@@ -2,25 +2,31 @@ defmodule LiveShowyWeb.Components.Forms.Input do
   @moduledoc false
   use Surface.Component
 
-  prop name, :string, required: true
-  prop value, :any, required: true
-  prop type, :string, default: "text"
   prop attrs, :map, default: %{}
   prop class, :css_class
-  prop min, :string
+  prop disabled, :boolean
   prop max, :string
+  prop min, :string
+  prop name, :string, required: true
+  prop placeholder, :string
+  prop readonly, :boolean
+  prop type, :string, default: "text"
+  prop value, :any, required: true
 
   def render(assigns) do
     ~F"""
     <input
+      {...@attrs}
+      {=@disabled}
+      {=@max}
+      {=@min}
+      {=@name}
+      {=@placeholder}
+      {=@readonly}
       {=@type}
       {=@value}
-      {=@name}
-      {=@min}
-      {=@max}
-      {...@attrs}
       class={
-        "bg-transparent transition focus:bg-white dark:focus:bg-default-900 rounded-sm w-auto font-mono px-1 py-0.5",
+        "bg-transparent transition focus:bg-white read-only:text-default-500 read-only:cursor-not-allowed disabled:cursor-not-allowed dark:focus:bg-default-900 rounded-sm w-auto flex-grow font-mono px-1 py-0.5",
         @class
       }
       />
