c459115759196b3e0eba3967a43b8c11bf6bba4e
Owen Bickford <owen+github@owencode.com>
2022-02-21 18:14:54 -0500

    add sizes, sort props

--- lib/live_showy_web/components/forms/button.ex
+++ lib/live_showy_web/components/forms/button.ex
@@ -6,16 +6,18 @@ defmodule LiveShowyWeb.Components.Forms.Button do
   prop attrs, :map, default: %{}
   prop class, :css_class, default: ""
   prop click, :event, required: true
+  prop disabled, :boolean, default: false
 
   prop kind, :string,
     default: "primary",
     values!: ~w(primary default)
 
+  prop name, :string
   prop label, :string
   prop rounded, :css_class, default: "rounded"
   prop shadow, :css_class, default: nil
+  prop size, :string, values!: ["sm", "base", "lg"], default: "base"
   prop type, :string, default: "button", values!: ~w(button submit reset)
-  prop disabled, :boolean, default: false
   prop value, :any
 
   slot default
@@ -23,22 +25,26 @@ defmodule LiveShowyWeb.Components.Forms.Button do
   def render(assigns) do
     ~F"""
     <button
-      type={@type}
-      disabled={@disabled}
+      {...@attrs}
+      {=@disabled}
+      {=@name}
+      :on-click={@click}
+      {=@type}
+      {=@value}
       class={
-        "px-2 py-1 uppercase transition select-none ring-4 ring-transparent font-bold active:translate-y-px focus:outline-none focus:z-50 hover:text-white",
+        "uppercase transition select-none ring-2 ring-transparent font-semibold active:translate-y-px focus:outline-none focus:z-50 hover:text-white",
         @class,
         @rounded,
         @shadow,
         "text-white": @active,
         "cursor-not-allowed opacity-70": @disabled,
+        "text-xs px-1.5 py-0.5": @size == "sm",
+        "text-base px-2 py-1": @size == "base",
+        "text-lg px-4 py-2": @size == "lg",
         "hover:bg-primary-500 dark:hover:bg-primary-600 focus:ring-primary-400": @kind == "primary",
         "bg-primary-500 dark:bg-primary-600": @kind == "primary" && @active,
         "bg-primary-200 text-primary-700 dark:bg-primary-800 dark:text-white": @kind == "primary" && !@active
       }
-      :on-click={@click}
-      value={@value}
-      {...@attrs}
     >
       <#slot>{@label}</#slot>
     </button>
